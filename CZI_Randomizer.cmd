09e7a0a8ca3792f4d407da2c84411c79d8eab9bf
ebioman <schmidemanuel@gmail.com>
2019-03-14 15:24:16 +0100

    Update CZI_Randomizer.cmd

--- CZI_Randomizer.cmd
+++ CZI_Randomizer.cmd
@@ -3,7 +3,8 @@ set folder=%date:/=%
 mkdir %folder%
 echo "Original","NEW" > %folder%\translationTable.csv
 for /F  "delims=" %%i in ('dir /b /s *.czi') do (
-	echo "%%i" , "!random!.czi" >> %folder%\translationTable.csv
-	xcopy /A /Y "%%i" %folder%\!random!.czi*
+	set myRandom=!random!
+	echo "%%i" , "!myRandom!.czi" >> %folder%\translationTable.csv
+	xcopy /A /Y "%%i" %folder%\!myRandom!.czi*
 )
 exit/B
