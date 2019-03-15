f8c6dfe1d8289fcf08ab277f9b77b4a7afb2c77b
ebioman <schmidemanuel@gmail.com>
2019-03-15 09:12:02 +0100

    Update CZI_fixer.cmd
    little bug evaluating spaces in filenames the  wrong way

--- CZI_fixer.cmd
+++ CZI_fixer.cmd
@@ -2,7 +2,7 @@ setlocal enabledelayedexpansion
 
 echo "file" , "md5" > associationTable.csv
 for /F  "delims=" %%i in ('dir /b /s *.czi') do (
-	call md5.bat %%i md5
+	call md5.bat "%%i" md5
 	set $myFile=%%~ni
 	echo !$myFile! , !md5! >> associationTable.csv
 )
